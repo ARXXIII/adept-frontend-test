bdd4653a45677a8f82cc85ffc474b282c70fec18
Андрей Романов <human1488pwnz@yandex.ru>
2024-11-13 21:54:22 +0300

    Change border-radius, font-weight, bg-colors Add variable classNames

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,17 +4,19 @@ type ButtonProps = {
     children: React.ReactNode
     type?: 'button' | 'submit'
     variant?: 'delete' | 'add'
+    className?: string
     onClick?: () => void
 }
 
-export const Button = ({ children, type, variant, onClick }: ButtonProps) => {
+export const Button = ({ children, type, variant, className, onClick }: ButtonProps) => {
     return (
         <button
             type={type || 'button'}
             onClick={onClick}
-            className={clsx("px-4 py-2 h-10 text-center text-sm hover:text-white border rounded-xl transition-all",
-                variant === 'add' && 'text-white border-green-500 bg-green-500',
-                variant === 'delete' && 'text-white border-red-500 bg-red-500'
+            className={clsx("px-4 py-2 text-center text-sm rounded-md shrink-0 transition-colors",
+                variant === 'add' && ' font-semibold bg-green-500 hover:bg-green-400',
+                variant === 'delete' && 'font-semibold bg-red-600 hover:bg-red-500',
+                className && className
             )}
         >
             {children}
