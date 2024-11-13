e9e702002cde2662b68279f1ec1a4d11840bec22
Андрей Романов <human1488pwnz@yandex.ru>
2024-11-13 14:30:30 +0300

    first commit

---
+++ src/components/button.tsx
@@ -0,0 +1,10 @@
+type ButtonProps = {
+    children: React.ReactNode
+    onClick?: () => void
+}
+
+export const Button = ({ children, onClick }: ButtonProps) => {
+    return (
+        <button onClick={onClick} className="p-2 text-center">{children}</button>
+    )
+}
