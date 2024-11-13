5e82f8fb5bd8609c6ebfcf315ded415840c9bd92
Андрей Романов <human1488pwnz@yandex.ru>
2024-11-13 17:56:33 +0300

    feat: custom button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,23 @@
+import clsx from "clsx"
+
 type ButtonProps = {
     children: React.ReactNode
+    type?: 'button' | 'submit'
+    variant?: 'delete' | 'add'
     onClick?: () => void
 }
 
-export const Button = ({ children, onClick }: ButtonProps) => {
+export const Button = ({ children, type, variant, onClick }: ButtonProps) => {
     return (
-        <button onClick={onClick} className="p-2 text-center">{children}</button>
+        <button
+            type={type || 'button'}
+            onClick={onClick}
+            className={clsx("px-4 py-2 h-10 text-center text-sm hover:text-white border rounded-xl transition-all",
+                variant === 'add' && 'text-white border-green-500 bg-green-500',
+                variant === 'delete' && 'text-white border-red-500 bg-red-500'
+            )}
+        >
+            {children}
+        </button>
     )
 }
