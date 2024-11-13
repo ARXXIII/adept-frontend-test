0bb13a54668a2ad94b01aa9c008a04f4ff25d4d9
Андрей Романов <human1488pwnz@yandex.ru>
2024-11-13 21:55:07 +0300

    Change logo Add links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,17 @@
+import { FaGithub, FaTelegram } from "react-icons/fa"
+
 export const Header = () => {
     return (
-        <header className="flex items-center gap-x-8 px-4 lg:px-60 py-4 shadow-lg">
-            <h1 className="font-bold text-3xl">Logo</h1>
+        <header className="flex justify-between items-center gap-x-24 sticky top-0 px-4 lg:px-60 py-4 text-white border-b border-b-gray-500 bg-neutral-950">
+            <h1 className="font-bold text-2xl">ARXXIII</h1>
+            <nav className="flex items-center gap-x-8">
+                <a href="https://github.com/ARXXIII/adept-frontend-test" target="_blank">
+                    <FaGithub className="size-6" />
+                </a>
+                <a href="https://t.me/arxxiii" target="_blank">
+                    <FaTelegram className="size-6" />
+                </a>
+            </nav>
         </header>
     )
 }
