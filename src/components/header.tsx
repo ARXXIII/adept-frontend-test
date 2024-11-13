6c6d837c9b925e14bc57b2bc7d4afa7f02eee997
Андрей Романов <human1488pwnz@yandex.ru>
2024-11-13 17:56:54 +0300

    Add responsive padding-x

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <header className="flex items-center gap-x-8 px-60 py-4 shadow-lg">
+        <header className="flex items-center gap-x-8 px-4 lg:px-60 py-4 shadow-lg">
             <h1 className="font-bold text-3xl">Logo</h1>
         </header>
     )
